726b7142ebf6db3251d1e71be71f9ba6578d7dca
W Anders <w@doubleu.codes>
2026-06-23 17:03:01 -0500

    fix: add interal http client to client stringer

--- internal/netbox/client.go
+++ internal/netbox/client.go
@@ -6,6 +6,7 @@ import (
 	"net/url"
 )
 
+// Client for interacting with the Netbox API.
 type Client struct {
 	*http.Client
 	NetboxURL *url.URL
@@ -13,13 +14,15 @@ type Client struct {
 	UserAgent string
 }
 
+// String returns a string representation of the Client with sensitive
+// information redacted.
 func (c *Client) String() string {
 	var tokenValue string
 	if c.HasToken() {
 		tokenValue = "[REDACTED]"
 	}
 	return fmt.Sprintf(
-		"Client{NetboxURL: %s, token: %s, UserAgent: %s, Client: %#v",
+		"Client{NetboxURL: %s, token: %s, UserAgent: %s, Client: %#v}",
 		c.NetboxURL,
 		tokenValue,
 		c.UserAgent,
@@ -27,10 +30,12 @@ func (c *Client) String() string {
 	)
 }
 
+// SetToken sets the token for the Client.
 func (c *Client) SetToken(token string) {
 	c.token = token
 }
 
+// HasToken returns true if the Client has a token set.
 func (c *Client) HasToken() bool {
 	return c.token != ""
 }
